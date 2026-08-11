f651e397d48ddc9cf7e1463132e4c3d1c9ba5b0a
Nhbab <124068266+Nhbab@users.noreply.github.com>
2026-08-11 21:29:48 +0700

    Add files via upload

---
+++ agent/model.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+
+class SimpleMLP(nn.Module):
+    def __init__(self, input_dim=20, hidden_dim=64, num_classes=2):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, num_classes)
+        )
+
+    def forward(self, x):
+        return self.net(x)
